f93459015fdeb16a359f0b143133102231b24bf7
aura70001 <b0232036@ems.niu.edu.tw>
2023-11-23 18:04:43 +0800

    1123

--- script/HW05.js
+++ script/HW05.js
@@ -25,20 +25,9 @@ let date = 0;
 
 yearList.addEventListener("change", refreshDate);
 monthList.addEventListener("change", refreshDate);
+// dateList.addEventListener("change", refresh);
 
 
-function refreshDate() {
-    year = yearList.value;
-    month = monthList.value;
-
-    let checkDate = new Date(year, month, 0); // 確認該年該月有幾天
-    date = checkDate.getDate();
-
-    dateList.innerHTML = ''; // 每次"日"都要清空，之後才寫入
-    for (let i = 1; i <= date; i++) {
-        dateList.innerHTML += `<Option value="${i}">${i}</Option>`;
-    }
-}
 
 // 3.建立月曆
 fillDate = document.getElementById("fillDate");
@@ -68,3 +57,56 @@ for (let i = 3; i <= 31; i++) {
 }
 Str1 += "</tr>"
 fillDate.innerHTML += Str1
+
+
+function refreshDate() {
+    year = yearList.value;
+    month = monthList.value;
+    today = dateList.value;
+    let checkDate = new Date(year, month, 0); // 確認該年該月有幾天
+    date = checkDate.getDate();
+
+    dateList.innerHTML = ''; // 每次"日"都要清空，之後才寫入
+    for (let i = 1; i <= date; i++) {
+        dateList.innerHTML += `<Option value="${i}">${i}</Option>`;
+    }
+
+    let firstDayOfWeek = new Date(year, month - 1, 1).getDay(); // 取得該月第一天是星期幾
+
+    fillDate.innerHTML = ""; // 清空月曆
+
+    let Str = "<tr>";
+    let initial = 1;
+
+    // 根據該月第一天是星期幾，來決定第一行空格
+    for (let i = 0; i < firstDayOfWeek; i++) {
+        Str += "<td></td>";
+        initial++;
+    }
+
+    // 接著填入日期
+    for (let i = 1; i <= date; i++) {
+        if (i == today) {
+            if (initial % 7 == 0) {
+                Str += `<td class="today">${i}</td></tr><tr>`;
+            } else {
+                Str += `<td class="today">${i}</td>`;
+            }
+        }
+        else if (initial % 7 === 0) {
+            Str += "<td>" + i + "</td></tr><tr>";
+        } else {
+            Str += "<td>" + i + "</td>";
+        }
+        initial++;
+    }
+
+    // 填入剩餘的空格
+    while (initial % 7 !== 0) {
+        Str += "<td></td>";
+        initial++;
+    }
+
+    Str += "</tr>";
+    fillDate.innerHTML = Str; // 更新月曆
+}
